5bc46c26c0ca16d92c035adbc77061c15d1f12b4
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2026-01-16 22:28:39 +0600

    project complete

--- app/src/main/java/com/example/bookshelf/MainActivity.kt
+++ app/src/main/java/com/example/bookshelf/MainActivity.kt
@@ -4,44 +4,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.bookshelf.ui.theme.BookshelfTheme
+import com.example.bookshelf.userinterface.BookshelfApp
+import com.example.bookshelf.userinterface.BookshelfViewModel
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: BookshelfViewModel by viewModels {
+        val app = application as BookshelfApplication
+        BookshelfViewModel.provideFactory(app.booksRepository)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            BookshelfTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            MaterialTheme {
+                BookshelfApp(
+                    uiState = viewModel.uiState,
+                    onRetry = { viewModel.loadBooks("jazz+history") }
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BookshelfTheme {
-        Greeting("Android")
     }
 }
